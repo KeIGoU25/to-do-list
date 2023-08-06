341b16b03b6d306c48e92ded2bfcaa08d1b545fa
KeIGoU25 <deejaymamplata29@gmail.com>
2023-08-06 09:17:21 +0800

    finish table task creation

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 let task = document.getElementById("task");
+let taskForm = document.getElementById("taskForm");
+let taskId = 1;
 
-task.addEventListener("submit", (event) => {
+taskForm.addEventListener("submit", (event) => {
     event.preventDefault();
+
+    let taskList = document.getElementById("taskList");
+
+    taskList.innerHTML += `
+        <tr>
+            <td>${taskId}</td>
+            <td>${task.value}</td>
+        </tr>
+    `;
+    taskId++;
+    task.value = null;
 })
