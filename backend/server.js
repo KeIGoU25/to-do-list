bd3f2c276371c2917a73ea744a02e2b33fe62b5d
KeIGoU25 <deejaymamplata29@gmail.com>
2023-08-07 11:09:21 +0800

    basic listing done

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,61 @@ const passwordDb = process.env.PASSWORDDB;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.post('/submit', (req, res) => {
+const { MongoClient } = require('mongodb');
+let crd = `${usernameDb} and ${passwordDb}`;
+const uri = `mongodb+srv://${usernameDb}:${passwordDb}@cluster0.4s3pmiy.mongodb.net/todoDb?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri);
+
+app.post('/submit', async (req, res) => {
+  const id = req.body.id;
   const task = req.body.task;
 
-  console.log('Task' + task);
-  res.send(task);
+  try {
+    await client.connect();
+    console.log("Connection success");
+
+    const db = client.db('todoDb'); // Reference the database
+    const taskCollection = db.collection('tasks'); // Reference a collection
+
+    // Insert a document into the collection
+    const result = await taskCollection.insertOne({
+      id: id,
+      task: task
+    });
+
+    console.log("Inserted:", result.insertedId);
+    res.status(200).send({id, task});
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error in saving task");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+    console.log("Close...");
+  }
+});
+
+async function retrieveData() {
+  const db = client.db('todoDb');
+  const collection = db.collection('tasks');
+
+  try {
+      await client.connect();
+
+      const query = {}; // You can customize your query here
+      const result = await collection.find(query).toArray();
+      return result;
+  } catch (err) {
+      console.error("Error retrieving data:", err);
+  } finally {
+      client.close();
+  }
+}
+
+app.get('/api/data', async (req, res) => {
+    const dataArray = await retrieveData();
+    res.json(dataArray);
 });
 
 app.listen(port, () => {
@@ -23,35 +73,5 @@ app.listen(port, () => {
     console.log(path.join(__dirname, '../public'));
 });
 
-// const { MongoClient } = require('mongodb');
-// let crd = `${usernameDb} and ${passwordDb}`;
-// const uri = `mongodb+srv://${usernameDb}:${passwordDb}@cluster0.4s3pmiy.mongodb.net/test?retryWrites=true&w=majority`;
 
-// const client = new MongoClient(uri);
 
-// async function run() {
-//   try {
-//     await client.connect();
-//     console.log("Connection success");
-
-//     const db = client.db('todoDb1'); // Reference the database
-//     const taskCollection = db.collection('tasks1'); // Reference a collection
-
-//     // Insert a document into the collection
-//     const result = await taskCollection.insertOne({
-//       title: 'Complete the project',
-//       status: 'In Progress'
-//     });
-
-//     console.log("Inserted:", result.insertedId);
-
-//   } catch (err) {
-//     console.error(err);
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//     console.log("Close...");
-//   }
-// }
-
-// run().catch(console.dir);
--- public/script.js
+++ public/script.js
@@ -2,27 +2,43 @@ let task = document.getElementById("task");
 let taskForm = document.getElementById("taskForm");
 let taskId = 1;
 
-const displayTask = (task) => {
+async function fetchData() {
+    try {
+        const response = await fetch('/api/data');
+        const data = await response.json();
+
+        data.forEach(item => {
+            displayTask(item.id, item.task);
+        });
+        
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+fetchData();
+
+const displayTask = (id, task) => {
     let taskList = document.getElementById("taskList");
 
     taskList.innerHTML += `
         <tr>
-            <td>${taskId}</td>
+            <td>${id}</td>
             <td>${task}</td>
         </tr>
     `;
-    taskId++;
 }
 
 $(document).ready(function () {
     $('#submitBtn').click(function (e) { 
         e.preventDefault();
         const task = $('#task').val();
-
-        $.post("/submit", { task: task },
-        function (task) { 
-            displayTask(task);
+        $.post("/submit", { id: taskId, task: task },
+        function (data) { 
+            const { id, task } = data;
+            displayTask(id, task);
             $('#task').val('');
+            taskId++;
         });       
     });
 });
